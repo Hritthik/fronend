aeb8d278d201b3e9c2ed25dc3e5f29f7bcea0a8a
Hritthik <royhritthik@gmail.com>
2023-05-03 05:10:38 +0530

    added fetures

---
+++ js/next_module.js
@@ -0,0 +1,33 @@
+    // <!-- <script>
+    //     (function (i, s, o, g, r, a, m) {
+    //         i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+    //             (i[r].q = i[r].q || []).push(arguments)
+    //         }, i[r].l = 1 * new Date(); a = s.createElement(o),
+    //             m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    //     })(window, document, 'script', '../www.google-analytics.com/analytics.js', 'ga');
+    //     ga('create', 'UA-77233126-1', 'auto');
+    //     ga('send', 'pageview');
+    // </script> -->
+    
+    // <!-- <script>
+    //     (function (w, d, s, l, i) {
+    //         w[l] = w[l] || []; w[l].push({
+    //             'gtm.start':
+    //                 new Date().getTime(), event: 'gtm.js'
+    //         }); var f = d.getElementsByTagName(s)[0],
+    //             j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+    //                 '../www.googletagmanager.com/gtm5445.html?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    //     })(window, document, 'script', 'dataLayer', 'GTM-MGQHQX5');
+    // </script> -->
+
+    // <!-- <script>
+    //     !function (e, t, n, s, u, a) {
+    //         e.twq || (s = e.twq = function () {
+    //             s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
+    //         }, s.version = '1.1', s.queue = [], u = t.createElement(n), u.async = !0, u.src = '../static.ads-twitter.com/uwt.js',
+    //             a = t.getElementsByTagName(n)[0], a.parentNode.insertBefore(u, a))
+    //     }(window, document, 'script');
+
+    //     twq('init', 'o4ora');
+    //     twq('track', 'PageView');
+    // </script> -->
